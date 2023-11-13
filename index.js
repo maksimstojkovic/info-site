8fb8d6e07edc55bf922b4416414db176e524bb5a
Maksim Stojkovic <18454392+maksimstojkovic@users.noreply.github.com>
2023-11-14 00:08:55 +1100

    Add server logic

---
+++ index.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+const http = require("http");
+
+const host = "0.0.0.0";
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+  const serve = (pathToFile) => {
+    fs.readFile(pathToFile, (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.end(data);
+      }
+    });
+  };
+
+  switch (req.url) {
+    case "/": {
+      serve("index.html");
+      break;
+    }
+    case "/about": {
+      serve("about.html");
+      break;
+    }
+    case "/contact-me": {
+      serve("contact-me.html");
+      break;
+    }
+    case "/style.css": {
+      serve("style.css");
+      break;
+    }
+    default: {
+      serve("404.html");
+    }
+  }
+});
+server.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
